263a29be32696ac9496bbaba759afa71b32a19cc
peterson and Carrie Schmid <peterson@>
2019-09-26 16:28:04 -0700

    Add functions for search and leave apartment.

--- models/game.cs
+++ models/game.cs
@@ -3,27 +3,29 @@ namespace CatGame
     public class Game 
     {
         public string PlayerName {get; set;}
-        public int HP {get; set;}
+   
         public int TurnCount {get; set;}
         public bool Dead {get; set;}
+        public int MissTrain {get; set;}
+
         
         public Game(string name)
         {
-            PlayerName = name;
-            HP = 25;
+            PlayerName = name;     
             TurnCount = 0;
             Dead = false;
+            MissTrain = 2;
         }
 
-        public void SetDead()
+        public void SetDeadTurn()
             {
-                if(HP <= 0 || TurnCount > 10)
+                if(TurnCount > 10)
                 {
                     Dead = true;
                 }
             }
         
-        public string ReturnDead()
+        public string ReturnDeadTurn()
             {
             if(Dead == true)
                 {
@@ -31,14 +33,19 @@ namespace CatGame
                 }
             else
                 {
-                    return "You are still livin'";
+                    return "";
                 }   
             }
 
-        public int TurnUp()
+        public void TurnUp()
         {
             TurnCount = TurnCount + 1;
-            return TurnCount;
+        }
+
+        public string MissTurnTrain()
+        {
+            TurnCount = TurnCount + MissTrain;
+            return $"You have {TurnCount} turns.";
         }
     }
     public class Inventory
@@ -46,43 +53,109 @@ namespace CatGame
         public bool Knife {get; set;}
         public bool DogBone {get; set;}
         public bool Watch {get; set;}
+        public bool HaveWaterBottle {get; set;}
 
         public Inventory()
         {
             Knife = false;
             DogBone = false;
             Watch = false;
+            HaveWaterBottle = false;
+
+        }
+        public void FindKnife()
+        {
+            Knife = true;
+        }
+        public void FindDogBone()
+        {
+            DogBone = true;
+        }
+        public void FindWatch()
+        {
+            Watch = true;
+        }
+
+        public void GetWaterBottle()
+        {
+            HaveWaterBottle = true;
         }
 
     }
     
-    public class Bonuses
+    
+    public class HealthEvents
     {
+
+        public int Mugger {get; set;}
+        public int PassOut {get; set;}
+        public int DogChase {get; set;}
         public int WaterBottle {get; set;}
         public int Sandwich {get; set;}
         public int FlyKite {get; set;}
+        public int HP {get; set;}
+        public bool Dead{get; set;}
 
-        public Bonuses()
+        public HealthEvents()
         {
-            WaterBottle = 5;
-            Sandwich = 5;
-            FlyKite = 5;
-        }
-
-    }
-    public class Events
-    {
-        public int Mugger {get; set;}
-        public int MissTrain {get; set;}
-        public int PassOut {get; set;}
-        public int DogChase {get; set;}
-
-        public Events()
-        {
-            Mugger = 15;
-            MissTrain = 2;
+            Mugger = 15;     
             PassOut = 20;
             DogChase = 10; 
+            WaterBottle = 25;
+            Sandwich = 5;
+            FlyKite = 5;
+            HP = 25;          
         } 
+        public string MuggerEvent()
+        {
+            HP = HP - Mugger;
+            return "Your health level is" + HP;
+        }
+        public string PassOutEvent()
+        {
+            HP = HP - PassOut;
+            return $"Your health level is {HP}"; 
+        }
+        public string DogChaseEvent()
+        {
+            HP = HP - DogChase;
+            return $"Your health level is {HP}";
+        }
+        public string DrinkWaterEvent()
+        {
+            HP = HP + 5;
+            WaterBottle = WaterBottle - 5;
+            return $"Your health level is {HP}";
+        }
+        public string EatSandwichEvent()
+        {
+            HP = HP + Sandwich;
+            return $"Your health level is {HP}";
+        }
+        public string FlyKiteEvent()
+        {
+            HP = HP + FlyKite;
+            return $"Your health level is {HP}";
+        }
+
+        public void SetDeadHP()
+            {
+                if(HP<=0)
+                {
+                    Dead = true;
+                }
+            }
+        
+        public string ReturnDeadHP()
+            {
+            if(Dead == true)
+                {
+                    return "You have died.";
+                }
+            else
+                {
+                    return "";
+                }   
+            }
     }
 }
--- program.cs
+++ program.cs
@@ -1,9 +1,122 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Game;
+using CatGame;
 
-namespace Game
+namespace CatGame
 {
-    
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Game newGame = new Game(name);
+
+            Inventory newInventory = new Inventory();
+            HealthEvents newHealthEvents = new HealthEvents();
+
+            Console.WriteLine("You awak from a slumber to a loud crash.  You look out your window and see a bright green shmear across the sky.");
+            Console.WriteLine("You feel your anxiety rise with the unfamiliar glow from the sky.  You look around your filthy apartment looking for the only thing you know will give you some releif.");
+            Console.WriteLine("After a quick search, you only notice your blood-stained sheetless mattress, your fridge thats mostly full of 90% empty dried condiments.  What you can't seem to find is your prized hairless cat, Fluffy, is nowhere to be seen.");
+            SearchApartment(newInventory);
+
+            LeaveApartment(newInventory, newHealthEvents);
+
+
+        }
+
+        public static void LeaveApartment(Inventory newInventory, HealthEvents newHealthEvents)
+        {
+            Console.WriteLine("You walk out onto the street.  The green glow illuminates the ground infront of you.  You hear a faint meow coming from the ally across the street.");
+            Console.WriteLine("Your phone dings.  You pull it out to find a reminder for your friends birthday party at the park.");
+            Console.WriteLine("Should you go to the park for a drink or investigate the ally?");
+            string chooseSorP = Console.ReadLine();
+            if (chooseSorP == "ally")
+            {
+                Console.WriteLine("You follow the meowing down the ally.  A homeless man asks you for a dollar.  You check your pockeets but don't have any money.  At the end of the ally there is the local subway stop.  You head down the stairs.");
+                Console.WriteLine("You turn a corner at the bottom of the stairs when you hear a familiar voice.  Its the bum from the ally! He pulls out nun-chucks and asks for all your money.");
+                if (newInventory.Knife == true)
+                {
+                    Console.WriteLine("You pull out the knife and slit his throat.");
+                }
+                else
+                {
+                    Console.WriteLine("The mugger attacks you, steals everything from you." + newHealthEvents.MuggerEvent());
+                    newInventory.HaveWaterBottle = false;
+                }
+
+
+                Console.WriteLine("Now that the mugger is dealt with do you want to search or catch a train?");
+                string searchORtrain = Console.ReadLine();
+                if(searchORtrain == "search")
+                {
+                    Console.WriteLine("You notice a murder of rats gnawing on a soggy dog bone.  Do you want to grab it? y/n");
+                    string bonegrab = Console.ReadLine();
+                    if(bonegrab == "y")
+                    {
+                        if (!newInventory.DogBone)
+                        {
+                            newInventory.FindDogBone();
+                            Console.WriteLine("the rats scurry away and you grab the soggy bone. your hand is gross now.");
+                        }
+                        else
+                        {
+                            
+                        }
+                    }
+                }
+
+            }
+            else if (chooseSorP == "park")
+            {
+                Console.WriteLine("");
+            }
+        }
+        public static void SearchApartment(Inventory newInventory)
+        {
+            Console.WriteLine("What would you like to do? search/leave");
+            string response = Console.ReadLine();
+            if (response == "search")
+            {
+                Console.WriteLine("Where would you like to search in the apartment?");
+                string apartmentSearch = Console.ReadLine();
+                if(apartmentSearch == "fridge")
+                {
+                    if(!newInventory.HaveWaterBottle)
+                    {
+                        Console.WriteLine("You found a water bottle! You can drink water to boost your health points.");
+                        newInventory.GetWaterBottle();
+                        SearchApartment(newInventory);
+                    }
+                    else
+                    {
+                        Console.WriteLine("You've already searched the fridge.");
+                        SearchApartment(newInventory);
+                    }
+                }
+                else if (apartmentSearch == "under bed")
+                {
+                    Console.WriteLine("Game over. You found Fluffy!");
+
+                }
+                else if (apartmentSearch == "bed")
+                {
+                    if(!newInventory.Knife)
+                    {
+                        Console.WriteLine("You find a knife in your bed? No wonder your sheetless mattress is blood-stained...");
+                    newInventory.FindKnife();
+                    SearchApartment(newInventory);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You don't find anything there.");
+                    SearchApartment(newInventory);
+                }
+
+            }
+
+        }
+    }
 }
